4b96d491cc30f89185db85d9799ee2a4244bfe53
athukuribhavani <bhavani.athukuri85@gmail.com>
2018-03-04 17:27:15 +0530

    java file added

---
+++ src/main/java/buildpipeline/pipeline1.java
@@ -0,0 +1,11 @@
+package buildpipeline;
+
+public class pipeline1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("   vbjbxjvjxcvj    ");
+		System.out.println("bhdbhdbhj");
+	}
+
+}
