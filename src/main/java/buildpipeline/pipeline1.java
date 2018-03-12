d730a914de6c6980916f1fd5c3af96729eb83497
athukuribhavani <bhavani.athukuri85@gmail.com>
2018-03-12 13:27:15 +0530

    m

--- src/main/java/buildpipeline/pipeline1.java
+++ src/main/java/buildpipeline/pipeline1.java
@@ -5,13 +5,6 @@ public class pipeline1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println(" hello");
-		System.out.println("java");
-		System.out.println("bsjbjas");
-		System.out.println("nbdj");
-		System.out.println("nnkkm");
-		System.out.println("bbjsbdjn");
-		System.out.println("msn fmn zm nmn");
 		
 
 }
